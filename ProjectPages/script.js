c92ea8f91736837511770f11ae9e713d75663d66
NathanLibo-on <b2029nalliboon@pshs.edu.ph>
2026-02-17 23:59:06 +0800

    commit

--- ProjectPages/script.js
+++ ProjectPages/script.js
@@ -1,5 +1,5 @@
 const chords = document.querySelectorAll(
-    ".cMAJOR, .cdMAJOR, .dMAJOR, .deMajor, .eMAJOR, .fMAJOR, .fgMAJOR, .gMAJOR, .gaMAJOR, .aMAJOR, .abMAJOR, .bMAJOR, \
+    ".cMAJOR, .cdMAJOR, .dMAJOR, .deMAJOR, .eMAJOR, .fMAJOR, .fgMAJOR, .gMAJOR, .gaMAJOR, .aMAJOR, .abMAJOR, .bMAJOR, \
      .cMINOR, .cdMINOR, .dMINOR, .deMINOR, .eMINOR, .fMINOR, .fgMINOR, .gMINOR, .gaMINOR, .aMINOR, .abMINOR, .bMINOR"
 );
 
